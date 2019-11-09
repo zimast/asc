869aa00427aa5bafbb50cf58412afd698117947c
zimast <zima.stefan.2016@gmail.com>
2019-11-09 15:33:49 +0100

    Finished Section 7 Lesson 45

--- server/security.utils.ts
+++ server/security.utils.ts
@@ -9,13 +9,13 @@ export const signJwt = util.promisify(jwt.sign);
 
 const RSA_PRIVATE_KEY = fs.readFileSync('./demos/private.key');
 const RSA_PUBLIC_KEY = fs.readFileSync('./demos/public.key');
-const SESSION_DURATION = 240;
+const SESSION_DURATION = 1000;
 
 export async function createSessionToken(userId: string) {
 
     return await signJwt({}, RSA_PRIVATE_KEY, {
         algorithm: 'RS256',
-        expiresIn: 240,
+        expiresIn: SESSION_DURATION,
         subject: userId
     });
 
