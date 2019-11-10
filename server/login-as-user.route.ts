a2a6e1b2b6a06dbd8b22b1aef5c8c7f662928cbe
zimast <zima.stefan.2016@gmail.com>
2019-11-10 16:46:36 +0100

    Finished Section 9 Lesson 68

--- server/login-as-user.route.ts
+++ server/login-as-user.route.ts
@@ -1,14 +1,27 @@
 import { Request, Response, NextFunction } from 'express';
 import { HTTP_STATUS_CODES } from './http-status-codes';
+import { database } from './database';
+import { createSessionToken } from './security.utils';
 
 export function loginAsUser(request: Request, response: Response, nextFunction: NextFunction) {
 
-    response
-        .status(HTTP_STATUS_CODES.success)
-        .json({
-            id: 1,
-            email: 'temp@gmail.com',
-            roles: ['STUDENT']
+    const impersonatedUserEmail = request.body.email;
+    const impersonatedUser = database.findUserByEmail(impersonatedUserEmail);
+
+    createSessionToken(impersonatedUser)
+        .then(sessionToken => {
+
+            response.cookie('SESSIONID', sessionToken, { httpOnly: true, secure: true });
+            response.status(HTTP_STATUS_CODES.success).json({
+                id: impersonatedUser.id,
+                email: impersonatedUser.email,
+                roles: impersonatedUser.roles
+            });
+
+        })
+        .catch(error => {
+            console.log('Error trying to log in as user: ', error);
+            response.sendStatus(HTTP_STATUS_CODES.internal_server_error);
         });
 
 }
